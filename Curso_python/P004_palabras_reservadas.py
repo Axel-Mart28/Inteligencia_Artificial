ca000db5150c879e8b632f9e3365d042a45b3507
Axel-Mart28 <axelxdramos10@gmail.com>
2025-09-17 00:42:16 -0600

    Palabras reservadas

---
+++ Curso_python/P004_palabras_reservadas.py
@@ -0,0 +1,5 @@
+#En este programa se muestra un ejemplo del uso de las palabras reservadas en Python
+
+"Al ejecutar las siguientes lineas, se muestran las palabras reservadas de Python en la consola:"
+import keyword
+print(keyword.kwlist)
