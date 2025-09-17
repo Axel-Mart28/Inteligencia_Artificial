2f40fa14c7cc8e60caf2404e61e4679abf024df2
Axel-Mart28 <axelxdramos10@gmail.com>
2025-09-17 00:32:51 -0600

    Nueva version

---
+++ Curso_python/P003_Tipos_de_datos.py
@@ -0,0 +1,9 @@
+"En este programa se ponen en practica los tipos de datos"
+
+saludo = "Hola mundo"  #Ejemplo del tipo de datos "String", que es una cadena de caracteres, o  un conjunto de caracteres
+edad = 32 #Ejemplo del tipo de dato "int", que es un tipo de dato que almacena mumeros enteros
+temperatura = -10 # Ejemplo del tipo de dato int, pero en negativo
+PI = 3.1416 #Ejemplo del tipo de dato "float", que sirve para almecanar numeros con punto decimal (para poner un numero decimal, debe ir ".", no ","")
+valor_negativo = -17.6273 #Ejemplo de tipo de dato float, pero negativo
+valor_boleano = True #Ejemplo de tipo de dato booleano, el cual solo puede representar el valor Verdadero o falso, para esta variable, se le asgina el valor de verdadero
+Valor_boleano = False #Ejemplo de tipo de dato booleano, el cual tiene el valor de Falso
