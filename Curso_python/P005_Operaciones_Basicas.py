c6e8d1c2f26139eb0b9bd221183855b1f76aef09
Axel-Mart28 <axelxdramos10@gmail.com>
2025-09-17 01:53:18 -0600

    Avance

---
+++ Curso_python/P005_Operaciones_Basicas.py
@@ -0,0 +1,39 @@
+"En el siguiente programa se muestra un ejemplo de las operaciones matematicas basicas en python"
+
+print(20 + 100) #Aqui, se imrpime en la consola el resultado de la operacion entre parentesis
+
+operacion = 20 + 50 # Se declara una variable llamada "operacion" la cual guarda el valor de la operacion de suma
+print(operacion)#Imprime el resultado de la variable
+
+operacion_resta = 10 - 4 #Se declara una variable llamada "operacion_resta" la cual guarda el valor de la operacion de resta
+print(operacion_resta)#Imprime el resultado de la variable
+
+operacion_mult = 10 * 4 #Se declara una variable llamada "operacion_mult" la cual guarda el valor de la operacion de multiplicacion
+print(operacion_mult)#Imprime el resultado de la variable
+
+operacion_division = 25 /5 #Se declara una variable llamada "operacion_division" la cual guarda el valor de la operacion de division
+print(operacion_division)#Imprime el resultado de la variable
+
+"Ejemplo con operaciones mixtas:"
+operacion_mixta = 100 * 2 + 8 / 4 - 9 + 7 * 14 * 5
+print(operacion_mixta)#Imprime el resultado de la variable
+
+numero1 = 100
+numero2 = 400
+
+resultado = numero1 + numero2
+
+print(resultado)
+
+numero3 = 10
+numero4 = 3
+
+cociente = numero1 / numero2
+resto = numero1 & numero2
+division_entera = numero3 // numero4
+
+print("Cociente:", cociente)
+print("Residuo:", resto)
+print("division_entera:", division_entera)
+
+
