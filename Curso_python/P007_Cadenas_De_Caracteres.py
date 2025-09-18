97e2830e63ecef02ee41e8962a9601373ccc80d3
Axel-Mart28 <axelxdramos10@gmail.com>
2025-09-18 17:42:37 -0600

    Primera version

---
+++ Curso_python/P007_Cadenas_De_Caracteres.py
@@ -0,0 +1,14 @@
+# pylint: disable=no-member, missing-module-docstring, trailing-whitespace
+# En el siguuente programa se muestran ejemplos de la concatenacion de caracteres
+
+fragmento1 = "La programacion es como la vida:"  #Primer fragmento de la palabra
+fragmento2 = " Llena de errores, pero tambien de posibilidades." #Segundo fragmento de la palabra
+
+frase_completa = fragmento1 + fragmento2 #Se hace la concatenacion (union de ambos fragmentos) y se guarda el resultado en la variable "frase_completa"
+print(frase_completa)  # Se muestra el resultado, en este caso la palabra completa
+
+#Diferentes maneras de mostrar la siguiente cadena de caracteres: "El tiempo es oro, me dijo"
+
+print('"El tiempo es oro", me dijo') 
+
+print("\"El tiempo es oro\", me dijo.")
